d66e9c35f9952255ce14cdad668c3a6015b400af
Jonathan <emeraldusk@gmail.com>
2023-08-06 19:51:16 -0300

    feat(azure): added public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,13 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = azurerm_resource_group.club-devops.name
+  location            = azurerm_resource_group.club-devops.location
+  allocation_method   = "Dynamic"
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.club-devops.location
@@ -52,6 +59,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "club-devops-ip"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
